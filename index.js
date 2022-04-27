46552b18bd9d3ac0110d89604937d7b4004d2ff7
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-04-27 12:32:52 -0700

    pushing throwaway

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log("server listening...");
+});
+
+server.get("/dao", (req, res) => {
+  res.send("<h1>Hi Dao, you are the best.</h1>");
+});
